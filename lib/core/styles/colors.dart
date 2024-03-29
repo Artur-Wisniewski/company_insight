40a6e56a3b1eba7c90a8014c4a6743a5640e8b3f
SweetCherry <artwis1998@gmail.com>
2024-03-29 18:12:53 +0100

    Add color palette

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const violet = Color(0xFF5E50FF);
+  static const darkViolet1 = Color(0xFF5647FF);
+  static const darkViolet2 = Color(0xFF32289A);
+  static const white = Color(0xFFFFFFFF);
+  static const grey = Color(0xFF8A8A8A);
+  static const darkGreyBlue = Color(0xFF2A313D);
+  static const darkGrey = Color(0xFF0A1013);
+  static const darkGrey2 = Color(0xFF020403);
+  static const black = Colors.black;
+  static const lime = Color(0xFF8DFF7E);
+  static const red = Color(0xFFFE4A4A);
+}
