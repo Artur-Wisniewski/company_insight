597f5e96f65582a0860288942774b0d65f6f26e5
SweetCherry <artwis1998@gmail.com>
2024-03-30 14:25:27 +0100

    Add icons assets

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -8,5 +8,20 @@ class AssetInfo {
 class SvgAssets {
   static const _imageDirectoryPath = 'assets/images';
 
-  static const AssetInfo blur = AssetInfo(path: '$_imageDirectoryPath/blur.svg', semanticsLabel: 'Blur');
+  static const bookmarkOutline =
+      AssetInfo(path: '$_imageDirectoryPath/bookmark_outline.svg', semanticsLabel: 'bookmark fill out icon');
+  static const bookmarkFill =
+      AssetInfo(path: '$_imageDirectoryPath/bookmark_fill.svg', semanticsLabel: 'bookmark fill icon');
+  static const chevronLeft =
+      AssetInfo(path: '$_imageDirectoryPath/chevron_left.svg', semanticsLabel: 'arrow left icon');
+  static const homeOutline =
+      AssetInfo(path: '$_imageDirectoryPath/home_outline.svg', semanticsLabel: 'home outline icon');
+  static const homeFill = AssetInfo(path: '$_imageDirectoryPath/home_fill.svg', semanticsLabel: 'home fill icon');
+  static const search = AssetInfo(path: '$_imageDirectoryPath/search.svg', semanticsLabel: 'search icon');
+  static const emptyBox = AssetInfo(path: '$_imageDirectoryPath/open_box.svg', semanticsLabel: 'empty box icon');
+  static const profileOutline =
+      AssetInfo(path: '$_imageDirectoryPath/profile_outline.svg', semanticsLabel: 'profile outline icon');
+  static const profileFill =
+      AssetInfo(path: '$_imageDirectoryPath/profile_fill.svg', semanticsLabel: 'profile fill icon');
+  static const trendingUp = AssetInfo(path: '$_imageDirectoryPath/trending_up.svg', semanticsLabel: 'trending up icon');
 }
