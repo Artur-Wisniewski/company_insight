df9d11b4693c7750e0d6cfac7a4714adf5b679b1
SweetCherry <artwis1998@gmail.com>
2024-03-30 14:18:20 +0100

    Add border Radii global style

---
+++ lib/core/styles/border_radii.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BorderRadii {
+  static const small = 8.0;
+  static const medium = 12.0;
+  static const large = 18.0;
+
+  static const BorderRadius largeLeft = BorderRadius.only(
+    topLeft: Radius.circular(BorderRadii.large),
+    bottomLeft: Radius.circular(BorderRadii.large),
+  );
+}
