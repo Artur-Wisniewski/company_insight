afdce743da839f2d0fe42d0bc85c4a9a2fef9199
SweetCherry <artwis1998@gmail.com>
2024-04-01 17:54:54 +0200

    Add loading cards to health tab

--- lib/features/search_companies/presentation/widgets/overview/financial_health_tab_view.dart
+++ lib/features/search_companies/presentation/widgets/overview/financial_health_tab_view.dart
@@ -1,4 +1,7 @@
+import 'package:company_insight_app/core/styles/border_radii.dart';
+import 'package:company_insight_app/core/styles/gaps.dart';
 import 'package:company_insight_app/core/styles/paddings.dart';
+import 'package:company_insight_app/core/widgets/app_shimmer.dart';
 import 'package:company_insight_app/core/widgets/modals/modals.dart';
 import 'package:company_insight_app/features/search_companies/presentation/widgets/something_went_wrong_info_block.dart';
 import 'package:company_insight_app/translations/l10n.dart';
@@ -17,36 +20,85 @@ class FinancialHealthTabView extends StatelessWidget {
       child: BlocBuilder<CompanyFinancialHealthCubit, CompanyFinancialHealthState>(builder: (context, state) {
         switch (state) {
           case CompanyFinancialHealthLoading _:
-            return const CircularProgressIndicator();
+            return _buildLoadingCards();
           case CompanyFinancialHealthDone stateDone:
-            return Padding(
-              padding: Paddings.largeAllExceptTop,
-              child: Column(
-                children: [
-                  FinancialHealthSection(
-                    title: L10n.current.netIncomeMargin,
-                    valuesOverTime: stateDone.netIncomeMarginsOverTime,
-                    onInfoPressed: () => Modals.showNetIncomeMarginInfo(context),
-                  ),
-                  FinancialHealthSection(
-                    title: L10n.current.currentRatio,
-                    valuesOverTime: stateDone.currentRatiosOverTime,
-                    onInfoPressed: () => Modals.showCurrentRatioInfo(context),
-                  ),
-                  FinancialHealthSection(
-                    title: L10n.current.debtToEquityRatio,
-                    valuesOverTime: stateDone.debtToEquityRatiosOverTime,
-                    onInfoPressed: () => Modals.showDebtToEquityRatio(context),
-                  ),
-                ],
-              ),
-            );
+            return _buildGraph(context, stateDone);
           case CompanyFinancialHealthFailure _:
             return const SomethingWentWrongInfoBlock();
           default:
             return const SizedBox();
         }
       }),
+    );
+  }
+
+  Widget _buildGraph(BuildContext context, CompanyFinancialHealthDone stateDone) {
+    return Padding(
+      padding: Paddings.largeAllExceptTop,
+      child: Column(
+        children: [
+          FinancialHealthSection(
+            title: L10n.current.netIncomeMargin,
+            valuesOverTime: stateDone.netIncomeMarginsOverTime,
+            onInfoPressed: () => Modals.showNetIncomeMarginInfo(context),
+          ),
+          FinancialHealthSection(
+            title: L10n.current.currentRatio,
+            valuesOverTime: stateDone.currentRatiosOverTime,
+            onInfoPressed: () => Modals.showCurrentRatioInfo(context),
+          ),
+          FinancialHealthSection(
+            title: L10n.current.debtToEquityRatio,
+            valuesOverTime: stateDone.debtToEquityRatiosOverTime,
+            onInfoPressed: () => Modals.showDebtToEquityRatio(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoadingCards() {
+    return Padding(
+      padding: Paddings.largeAllExceptTop,
+      child: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadii.mediumAll,
+            ),
+            child: const AppShimmer(
+              child: SizedBox(
+                width: double.infinity,
+                height: 200,
+              ),
+            ),
+          ),
+          Gaps.medium,
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadii.mediumAll,
+            ),
+            child: const AppShimmer(
+              child: SizedBox(
+                width: double.infinity,
+                height: 200,
+              ),
+            ),
+          ),
+          Gaps.medium,
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadii.mediumAll,
+            ),
+            child: const AppShimmer(
+              child: SizedBox(
+                width: double.infinity,
+                height: 200,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
